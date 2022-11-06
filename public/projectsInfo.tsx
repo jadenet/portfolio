55b2b02f15e125b47157f11e2ecd1e17c9815ec4
jadenet <97551221+jadenet@users.noreply.github.com>
2022-11-06 16:41:33 -0500

    Update projectsInfo.tsx

--- public/projectsInfo.tsx
+++ public/projectsInfo.tsx
@@ -1,7 +1,7 @@
 const Projects = [
   {
-    "name": "1",
-    "description": "hiiiiii",
+    "name": "Project 1",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc in massa sodales, sodales tellus eget, malesuada dolor. Nunc in metus posuere, vehicula nunc sit amet, egestas nisl. Fusce pharetra leo non purus lobortis congue. Suspendisse eget pretium arcu. Aliquam erat volutpat. Duis efficitur lorem nec imperdiet fringilla.",
     "date": { year: 2022, month: 8 },
     "tools": ["React", "NextJS", "Tailwind"],
     "demo": "",
@@ -9,7 +9,7 @@ const Projects = [
     "image": "https://imgs.search.brave.com/URIFScfOxp_htEumsqlVjCJP-LQAX9Z5uwmotdi527A/rs:fit:538:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5s/NllYejljaVRnWEsz/MkgtN3N1TkpnSGFH/aCZwaWQ9QXBp"
   },
   {
-    "name": "2",
+    "name": "Project 2",
     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc in massa sodales, sodales tellus eget, malesuada dolor. Nunc in metus posuere, vehicula nunc sit amet, egestas nisl. Fusce pharetra leo non purus lobortis congue. Suspendisse eget pretium arcu. Aliquam erat volutpat. Duis efficitur lorem nec imperdiet fringilla.",
     "date": { year: 2022, month: 9 },
     "tools": ["React", "NextJS"],
@@ -18,7 +18,7 @@ const Projects = [
     "image": "https://imgs.search.brave.com/URIFScfOxp_htEumsqlVjCJP-LQAX9Z5uwmotdi527A/rs:fit:538:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5s/NllYejljaVRnWEsz/MkgtN3N1TkpnSGFH/aCZwaWQ9QXBp"
   },
   {
-    "name": "3",
+    "name": "Project 3",
     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc in massa sodales, sodales tellus eget, malesuada dolor. Nunc in metus posuere, vehicula nunc sit amet, egestas nisl. Fusce pharetra leo non purus lobortis congue. Suspendisse eget pretium arcu. Aliquam erat volutpat. Duis efficitur lorem nec imperdiet fringilla.",
     "date": { year: 2022, month: 10 },
     "tools": ["SCSS"],
@@ -27,7 +27,7 @@ const Projects = [
     "image": "https://imgs.search.brave.com/URIFScfOxp_htEumsqlVjCJP-LQAX9Z5uwmotdi527A/rs:fit:538:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5s/NllYejljaVRnWEsz/MkgtN3N1TkpnSGFH/aCZwaWQ9QXBp"
   },
   {
-    "name": "4",
+    "name": "Project 4",
     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc in massa sodales, sodales tellus eget, malesuada dolor. Nunc in metus posuere, vehicula nunc sit amet, egestas nisl. Fusce pharetra leo non purus lobortis congue. Suspendisse eget pretium arcu. Aliquam erat volutpat. Duis efficitur lorem nec imperdiet fringilla.",
     "date": { year: 2022, month: 11 },
     "tools": ["NextJS", "Tailwind"],
