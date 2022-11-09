1e6e255f143330bef9c767b13492d2975b2a03ef
jadenet <97551221+jadenet@users.noreply.github.com>
2022-11-08 21:04:58 -0500

    Use next/image

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,3 +1,5 @@
+import Image, { StaticImageData } from "next/image";
+
 interface ProjectProps {
     name: string;
     date: { year: number; month: number };
@@ -5,7 +7,7 @@ interface ProjectProps {
     description: string;
     demo?: string;
     source: string;
-    image: string;
+    image: StaticImageData;
 }
 
 function Project(props: ProjectProps) {
@@ -14,8 +16,8 @@ function Project(props: ProjectProps) {
     return (
         <article className="flex flex-col w-5/6 lg:w-3/5 md:flex-row bg-white text-black rounded-xl">
             <div className="relative md:w-3/5 overflow-hidden rounded-t-xl md:rounded-tr-none md:rounded-bl-xl bg-gray-900">
-                <img aria-hidden src={props.image} alt="Project image" className="absolute w-full h-full object-cover blur-md scale-105" />
-                <img src={props.image} alt="Project image" className="absolute w-full h-full object-contain" />
+                <Image aria-hidden src={props.image} layout="fill" alt="Project image" className="absolute w-full h-full object-cover blur-md scale-105" />
+                <Image src={props.image} layout="fill" alt="Project image" className="absolute w-full h-full object-contain" />
             </div>
             <div className="flex flex-col justify-between gap-5 p-6 md:w-2/5">
                 <div className="flex flex-col gap-5">
@@ -30,8 +32,8 @@ function Project(props: ProjectProps) {
                 </div>
 
                 <div className="flex items-center gap-2 justify-end">
-                    {props.demo != null && <a href={props.demo} className="font-bold py-2 px-5 rounded-lg hover:scale-105 active:scale-95 transition border-2 text-sm text-blue-400 border-blue-400">Demo</a>}
-                    {props.source != null && <a href={props.source} className="font-bold py-2 px-5 rounded-lg hover:scale-105 active:scale-95 transition border-2 text-sm text-white bg-blue-400 border-blue-400">Source Code</a>}
+                    {props.demo != null && <a href={props.demo} target="_blank" className="font-bold py-2 px-5 rounded-lg hover:scale-105 active:scale-95 transition border-2 text-sm text-blue-400 border-blue-400">Demo</a>}
+                    {props.source != null && <a href={props.source} target="_blank" className="font-bold py-2 px-5 rounded-lg hover:scale-105 active:scale-95 transition border-2 text-sm text-white bg-blue-400 border-blue-400">Source Code</a>}
                 </div>
             </div>
         </article>
