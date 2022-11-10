3c6b6bdd39c8c1b57914a9322e79d2536c308d0e
jadenet <97551221+jadenet@users.noreply.github.com>
2022-11-10 09:55:45 -0500

    Remove HTML and CSS from tools

--- public/projectsInfo.tsx
+++ public/projectsInfo.tsx
@@ -8,7 +8,7 @@ const Projects = [
     "name": "portfolio",
     "description": "This website! After getting inspiration from other portfolio websites, I decided to make my own.",
     "date": { year: 2022, month: 9 },
-    "tools": ["HTML", "CSS", "TypeScript", "React", "NextJS", "Tailwind"],
+    "tools": ["TypeScript", "React", "NextJS", "Tailwind"],
     "demo": null,
     "source": "https://github.com/jadenet/portfolio",
     "image": portfolio
@@ -17,7 +17,7 @@ const Projects = [
     "name": "i-luminate",
     "description": "Another challenge created for fun. This was my first time using frameworks and utilizing a database. Although it turns out I didn't actually need a database for this project, it taught me more nonetheless.",
     "date": { year: 2022, month: 6 },
-    "tools": ["HTML", "CSS", "Svelte", "TypeScript", "Tailwind"],
+    "tools": ["Svelte", "TypeScript", "Tailwind"],
     "demo": null,
     "source": "https://github.com/jadenet/i-luminate",
     "image": iLuminate
@@ -26,7 +26,7 @@ const Projects = [
     "name": "iScream",
     "description": "A friend suggested the idea and I wanted to try out html and css.",
     "date": { year: 2022, month: 4 },
-    "tools": ["HTML", "CSS"],
+    "tools": [],
     "demo": "https://jadenet.github.io/iScream/",
     "source": "https://github.com/jadenet/iScream",
     "image": iScream
@@ -35,7 +35,7 @@ const Projects = [
     "name": "2 Hour Front Page",
     "description": "The first website I've made! At the time I didn't know about responsiveness so it's best to not look at this on mobile. :>",
     "date": { year: 2022, month: 3 },
-    "tools": ["HTML", "CSS"],
+    "tools": [],
     "demo": "https://jadenet.github.io/2-Hour-Front-Page/",
     "source": "https://github.com/jadenet/2-Hour-Front-Page",
     "image": TwoHourFrontPage
