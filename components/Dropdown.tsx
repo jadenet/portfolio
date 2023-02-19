c9327564ea3f5ea13f668506ce13411486130a96
Jaden <97551221+jadenet@users.noreply.github.com>
2023-02-19 13:33:56 -0500

    Change dropdown html to details tag

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from "react";
+import { ReactNode } from "react";
 
 interface dropdownProps {
     text: string;
@@ -6,15 +6,13 @@ interface dropdownProps {
 }
 
 function Dropdown(props: dropdownProps) {
-    const [display, setDisplay] = useState("hidden");
-
     return (
-        <div className="flex flex-col items-center relative">
-            <button onClick={() => { setDisplay(display === "block" ? "hidden" : "block") }} className="py-2 hover:scale-105 active:scale-95 transition text-black">{props.text}</button>
-            <div className={`${display} absolute z-10 border-2 border-gray-800 top-full bg-white rounded-md flex flex-col items-center`}>
+        <details className="flex flex-col items-center relative">
+            <summary className="py-2 hover:scale-105 active:scale-95 transition text-black">{props.text}</summary>
+            <div className="absolute z-10 border-2 border-gray-800 top-full bg-white rounded-md flex flex-col items-center">
                 {props.children}
             </div>
-        </div>
+        </details>
     )
 }
 
