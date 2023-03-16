4905dc27833e6603c2cca8c96300c21a3d8dc061
Jaden <97551221+jadenet@users.noreply.github.com>
2023-03-16 13:54:13 -0400

    Remove non allowed character

--- public/projectsInfo.tsx
+++ public/projectsInfo.tsx
@@ -7,18 +7,7 @@ import theNewOil from "./images/projects/The New Oil.png";
 const Projects = [
   {
     name: "The New Oil",
-    description: (
-      <p>
-        In a collaboration with{" "}
-        <a href="https://www.rikdenbreejen.nl">
-          Rik
-        </a>
-        , we redesigned and developed a website to improve UI and
-        maintainability. We were given a challenge to include no client-side
-        javascript per request, so we went with a static site generator that
-        could build the code of the website containing no javascript.
-      </p>
-    ),
+    description: "In a collaboration with Rik (https://www.rikdenbreejen.nl), we redesigned and developed a website to improve UI and maintainability. We were given a challenge to include no client-side javascript per request, so we went with a static site generator that could build the code of the website containing no javascript.",
     date: { year: 2023, month: 1 },
     tools: ["HTML", "CSS", "Astro", "Tailwind"],
     demo: "https://thenewoil.org",
@@ -27,12 +16,7 @@ const Projects = [
   },
   {
     name: "portfolio",
-    description: (
-      <p>
-        This website! After getting inspiration from other portfolio websites, I
-        decided to make my own.
-      </p>
-    ),
+    description: "This website! After getting inspiration from other portfolio websites, I decided to make my own.",
     date: { year: 2022, month: 9 },
     tools: ["HTML", "CSS", "TypeScript", "React", "NextJS", "Tailwind"],
     demo: "https://jadenet.github.io/portfolio",
@@ -41,14 +25,7 @@ const Projects = [
   },
   {
     name: "i-luminate",
-    description: (
-      <p>
-        Another challenge created for fun. This was my first time using
-        frameworks and utilizing a database. Although it turns out I didn't
-        actually need a database for this project, it taught me more
-        nonetheless.
-      </p>
-    ),
+    description: "Another challenge created for fun. This was my first time using frameworks and utilizing a database. Although it turns out I didn't actually need a database for this project, it taught me more nonetheless.",
     date: { year: 2022, month: 6 },
     tools: ["HTML", "CSS", "Svelte", "TypeScript", "Tailwind", "Supabase"],
     demo: null,
@@ -57,9 +34,7 @@ const Projects = [
   },
   {
     name: "iScream",
-    description: (
-      <p>A friend suggested the idea and I wanted to try out html and css.</p>
-    ),
+    description: "A friend suggested the idea and I wanted to try out html and css.",
     date: { year: 2022, month: 4 },
     tools: ["HTML", "CSS"],
     demo: "https://jadenet.github.io/iScream/",
@@ -68,12 +43,7 @@ const Projects = [
   },
   {
     name: "2 Hour Front Page",
-    description: (
-      <p>
-        The first website I've made! At the time I didn't know about
-        responsiveness so it's best to not look at this on mobile. :>
-      </p>
-    ),
+    description: "The first website I've made! At the time I didn't know about responsiveness so it's best to not look at this on mobile.",
     date: { year: 2022, month: 3 },
     tools: ["HTML", "CSS"],
     demo: "https://jadenet.github.io/2-Hour-Front-Page/",
